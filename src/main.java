b507defd0abd69d44129d567d7d3c819807e6a27
Nikitha-Anand <nikitha.rubesh@gmail.com>
2024-12-04 12:29:29 +0530

    Create main.java

---
+++ src/main.java
@@ -0,0 +1,15 @@
+public class main {
+    public static void main(String[] args){
+        Vehicle Car = new Vehicle("Tiger", 100, 20);
+
+        Helicopter helicopter = new Helicopter("Lion", 350, 40, 3, 100);
+
+        Car.IncreaseSpeed();
+        Car.IncreaseSpeed();
+        Car.PrintVehicle();
+
+        helicopter.IncreaseSpeed();
+        helicopter.IncreaseSpeed();
+        helicopter.PrintVehicle();
+    }
+}
